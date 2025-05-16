1a001dbb1cd4cf4080e596259fc8819f8b605b3f
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2025-05-16 10:20:16 +0100

    fix more relative import issues

--- src/passwords/passwords.module.ts
+++ src/passwords/passwords.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PasswordsService } from './passwords.service';
 import { PasswordsController } from './passwords.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { CryptModule } from 'src/crypt/crypt.module';
+import { CryptModule } from '../crypt/crypt.module';
 
 @Module({
   imports: [PrismaModule, CryptModule],
